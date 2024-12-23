312c07bd9aea85942f59ec4d0c25996723f6dddb
CHERUKURI KALYAN <svnkalyan@gmail.com>
2024-12-22 20:49:25 -0600

    added secomnd file Example1.py for practicing

--- Udemy/Example1.py
+++ Udemy/Example1.py
@@ -3,39 +3,36 @@ class Node:
         self.value = value
         self.next = None
 
-
 class LinkedList:
     def __init__(self, value):
         new_node = Node(value)
         self.head = new_node
         self.tail = new_node
         self.length = 1
-    
+
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
 
-    def append(self,value):
+    def append(self, value):
         new_node = Node(value)
-        if self.head == None:
+        if self.head is None:
             self.head = new_node
             self.tail = new_node
         else:
             self.tail.next = new_node
             self.tail = new_node
-        self.length += 1
+            
+        self.length+=1
         return True
+    
+
 
 my_linked_list = LinkedList(1)
 
 my_linked_list.append(2)
 
 my_linked_list.print_list()
-
-
-
-
-
 
